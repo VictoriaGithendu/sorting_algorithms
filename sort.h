ce3a252607bc7a9ea99edd3719ded0c2093476a1
VictoriaGithendu <victoriagithendu@gmail.com>
2023-12-20 00:47:36 -0800

    sorting algorithms

--- sort.h
+++ sort.h
@@ -14,6 +14,17 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * enum bool -  Enumeration of boolean value
+ * @false: equals 0
+ * @true: equals 1
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
@@ -29,5 +40,16 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker);
+void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker);
+void cocktail_sort_list(listint_t **list);
+int get_max(int *array, int size);
+void counting_sort(int *array, size_t size);
+void radix_counting_sort(int *array, size_t size, int sig, int *buff);
+void swap_ints(int *a, int *b);
+void bitonic_merge(int *array, size_t size, size_t start, size_t seq, char flow);
+void bitonic_seq(int *array, size_t size, size_t start, size_t seq, char flow);
+int hoare_partition(int *array, size_t size, int left, int right);
+void hoare_sort(int *array, size_t size, int left, int right);
 
 #endif
